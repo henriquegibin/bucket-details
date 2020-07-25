767b6753d0f10c3783ccead853b267714eb02c02
Henrique Gibin Ribeiro <henrique.gibin96@gmail.com>
2020-07-25 16:08:34 -0300

    improvments in errorchecker package

--- src/errorcheck/generic_error_checker.go
+++ src/errorcheck/generic_error_checker.go
@@ -4,16 +4,16 @@ import "log"
 
 // CheckFatal Receive one error object and check if something go wrong
 // and stop the execution
-func CheckFatal(e error) {
+func CheckFatal(e error, functionName string) {
 	if e != nil {
-		log.Fatal(e)
+		log.Fatalf("Function where error appear: %s\nError: %v", functionName, e)
 	}
 }
 
 // CheckError Receive one error object and check if something
 // go wrong, log the error, but don`t stop the execution
-func CheckError(e error) {
+func CheckError(e error, functionName string) {
 	if e != nil {
-		log.Println(e)
+		log.Printf("Function where error appear: %s\nError: %v", functionName, e)
 	}
 }
--- src/errorcheck/generic_error_checker_test.go
+++ src/errorcheck/generic_error_checker_test.go
@@ -7,18 +7,19 @@ import (
 
 func TestCheckError(t *testing.T) {
 	type args struct {
-		e error
+		e            error
+		functionName string
 	}
 
 	tests := []struct {
 		name string
 		args args
 	}{
-		{"Check if error is not nil and log the message", args{errors.New("failed")}},
+		{"Check if error is not nil and log the message", args{errors.New("failed"), "functionName"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			CheckError(tt.args.e)
+			CheckError(tt.args.e, tt.args.functionName)
 		})
 	}
 }
